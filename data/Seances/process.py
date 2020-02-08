e3216733ddc0a6cabe37b82c4416b299cc4f6896
Clement POIRET <clement.poiret@etu.univ-rouen.fr>
2020-02-08 17:22:34 +0100

    Added data

--- data/Seances/process.py
+++ data/Seances/process.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_excel("./20200205.xlsx")
+dataset = pd.read_excel("./20200206.xlsx")
 ind = dataset[dataset.Nom.astype(str).str.isdigit()].index
 dataset = dataset.drop(ind)
 
-dataset.to_excel("./20200205.xlsx", index=False)
+dataset.to_excel("./20200206.xlsx", index=False)
